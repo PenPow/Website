f962215a5a0d1164ab681972867c5901dd5012de
PenPow <josh@penpow.dev>
2023-02-14 17:08:05 +0000

    feat(error): new error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,13 +3,18 @@
 </script>
 
 <div class="max-w-3xl px-8 mt-24 mx-auto">
-	<h1 class="mt-4 text-3xl font-bold flex md:items-end md:flex-row flex-col">
-		<div>404 - Tea Not Found</div>
+	<h1 class="mt-4 text-4xl font-bold flex md:items-end md:flex-row flex-col">
+		418: I'm a Little Teapot 🫖
 	</h1>
+	<p class="mt-4 prose prose-base">
+		Short and Stout, here is my handle, here is my spout. When I get all steamed up, hear me shout. Tip me over and pour me out!
+	</p>
 
 	<Divider />
 
 	<p>
-		How did you get here? The page you tried to visit doesn't exist.
+		I'm not too sure how you ended up here, but while you are here, enjoy this picture of a perfect cup of tea.
+
+		<img src="/img/418-error.jpg" alt="cup of tea" class="mt-5 ml-2 h-96 rounded-md" />
 	</p>
 </div>
