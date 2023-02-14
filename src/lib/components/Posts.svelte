eb8a35d31e999fb00f6a20e75f6b890249c5ec12
PenPow <josh@penpow.dev>
2023-02-14 10:34:00 +0000

    feat(projects): show all projects button (#89)

--- src/lib/components/Posts.svelte
+++ src/lib/components/Posts.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { fetchMarkdownPosts } from "$lib/utils/fetchPosts";
 	import { onMount } from "svelte";
+	import { slide } from 'svelte/transition';
 	import Post from "./internal/Post.svelte";
 
 	export let all: "no" | "yes" = "no";
@@ -22,6 +23,8 @@
 	onMount(async () => {
 		posts = await fetchMarkdownPosts();
 	})
+
+	let showAllPosts = false;
 </script>
 
 {#if all === "no"}
@@ -37,6 +40,24 @@
 				<Post slug={post.path} title={post.meta.title} cover={post.meta.cover} description={post.meta.description} />
 			{/if}
 		{/each}
+
+		<button class="mt-6 ease-in-out transition-300 mb-5" on:click={() => showAllPosts = !showAllPosts}>
+			{#if !showAllPosts}
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="inline" width=16 height=16><path d="M201.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 338.7 54.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg> Show All Posts
+			{:else}
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width=16 height=16 class="inline transition duration-300 transform rotate-180"><path d="M201.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 338.7 54.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg> Show All Posts
+			{/if}
+		</button>
+
+		{#if showAllPosts}
+			{#each posts as post}
+				{#if !post.meta.major}
+					<div class="hover:scale-105 transform ease-in-out duration-300" transition:slide>
+						<Post slug={post.path} title={post.meta.title} cover={post.meta.cover} description={post.meta.description} />
+					</div>
+				{/if}
+			{/each}
+		{/if}
 	{:else}
 		{#each posts as post}
 			<Post slug={post.path} title={post.meta.title} cover={post.meta.cover} description={post.meta.description} />
--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -1,22 +1,25 @@
 <script lang="ts">
 	import Project from "$lib/components/internal/Project.svelte";
+	import { slide } from 'svelte/transition';
 
-	interface Project {
+	interface IProject {
 		link: string;
 		icon: string;
 		name: string;
 		color: string;
 		description: string;
+		major: boolean;
 		year: string | null;
 	}
 
-	const projects: Project[] = [
+	const projects: IProject[] = [
 		{
 			icon: "presenter.png",
 			name: "Presenter",
 			link: "about:blank",
 			year: "In Development",
 			color: "bg-purple-400",
+			major: false,
 			description: "A new form of powerpoint, designed for rapidly building code oriented presentations in your terminal.",
 		},
 		{
@@ -25,6 +28,7 @@
 			link: "about:blank",
 			year: "January 2023 - Present",
 			color: "bg-slate-400",
+			major: true,
 			description: "Freelance project that syncs data between Roblox, Discord and Postgres to share user information between servers",
 		},
 		{
@@ -33,6 +37,7 @@
 			link: "https://github.com/PenPow/Sentry",
 			year: "June 2022 - Present",
 			color: "bg-emerald-300",
+			major: true,
 			description: "Discord's Antivirus! Filter your server with on demand file scanning",
 		},
 		{
@@ -41,14 +46,37 @@
 			link: "about:blank",
 			year: "June 2020 - Febrary 2023",
 			color: "bg-blue-400",
+			major: false,
 			description: "Now shut down due to a lack of time to contribute, </design> was a 600 member design community that I helped administrate, focusing on providing a welcoming community for web development.",
 		}
 	];
+
+	let showAllProjects = false;
 </script>
 
 <h1 class="mt-4 text-2xl font-bold">My Work</h1>
 <div class="flex flex-col mt-2">
 	{#each projects as project}
-		<Project link={project.link} icon={project.icon} name={project.name} description={project.description} year={project.year} color={project.color} />
+			{#if project.major}
+				<Project link={project.link} icon={project.icon} name={project.name} description={project.description} year={project.year} color={project.color} />
+			{/if}
 	{/each}
+
+	<button class="mt-6 ease-in-out transition-300" on:click={() => showAllProjects = !showAllProjects}>
+		{#if !showAllProjects}
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="inline" width=16 height=16><path d="M201.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 338.7 54.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg> Show All Projects
+		{:else}
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width=16 height=16 class="inline transition duration-300 transform rotate-180"><path d="M201.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 338.7 54.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg> Show All Projects
+		{/if}
+	</button>
+
+		{#if showAllProjects}
+			{#each projects as project}
+				{#if !project.major}
+					<div class="hover:scale-105 transform ease-in-out duration-300" transition:slide>
+						<Project link={project.link} icon={project.icon} name={project.name} description={project.description} year={project.year} color={""} />
+					</div>
+				{/if}
+			{/each}
+		{/if}
 </div>
