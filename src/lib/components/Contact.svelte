3c494e865a30a2d3184be6241052ea95e1d36e57
PenPow <josh@penpow.dev>
2023-02-15 11:07:59 +0000

    fix(contact): align left if on mobile

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -7,5 +7,5 @@
 	<a href="mailto:josh@penpow.dev" target="_blank" rel="noreferrer" class="bg-yellow-500 bg-opacity-30 flex-1 md:mr-4 px-8 mt-6 py-4 rounded-md hover:scale-105 transform ease-in-out duration-300">📧 josh@penpow.dev</a>
 </div>
 <div class="flex flex-col md:flex-row justify-center">
-	<a href="https://github.com/PenPow" target="_blank" rel="noreferrer" class="bg-green-400 text-center bg-opacity-30 flex-1 md:mr-4 px-8 mt-6 py-4 rounded-md hover:scale-105 transform ease-in-out duration-300">🐙 PenPow</a>
+	<a href="https://github.com/PenPow" target="_blank" rel="noreferrer" class="bg-green-400 md:text-center text-left bg-opacity-30 flex-1 md:mr-4 px-8 mt-6 py-4 rounded-md hover:scale-105 transform ease-in-out duration-300">🐙 PenPow</a>
 </div>
