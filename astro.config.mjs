c3846e5762ab6df89746f2c1f959265de00ba47e
PenPow <josh@penpow.dev>
2024-01-03 14:42:15 +0000

    fix: actually remove markdoc
    this is cutting into my monthly build limit this is

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import robotsTxt from "astro-robots-txt";
-import markdoc from "@astrojs/markdoc";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://www.penpow.dev",
   integrations: [icon(), robotsTxt(), markdoc(), sitemap(), tailwind(), compress()],
