ee9763522a87cc908b080b5d899ac8a1fa67327f
PenPow <josh@penpow.dev>
2024-01-28 13:45:31 +0000

    fix: update rss schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { rssSchema } from '@astrojs/rss';
 
 const postsCollection = defineCollection({
 	type: 'content',
-	schema: rssSchema.extend({ hidden: z.boolean().optional().default(false), description: z.string() }),
+	schema: rssSchema.extend({ hidden: z.boolean().optional().default(false), description: z.string(), title: z.string(), pubDate: z.date() }),
 });
 
 export const collections = {
