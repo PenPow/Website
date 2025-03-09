4efb7ee63565dd58cfcfb70caf71da200c9271e1
Joshua Clements <josh@penpow.dev>
2025-03-09 13:28:17 +0000

    refactor: improve formatting

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,7 +4,12 @@ import { glob } from 'astro/loaders';
 
 const postsCollection = defineCollection({
 	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/posts" }),
-	schema: rssSchema.extend({ hidden: z.boolean().optional().default(false), description: z.string(), title: z.string(), pubDate: z.date() }),
+	schema: rssSchema.extend({ 
+		hidden: z.boolean().optional().default(false),
+		description: z.string(),
+		title: z.string(),
+		pubDate: z.date() 
+	}),
 });
 
 export const collections = {
